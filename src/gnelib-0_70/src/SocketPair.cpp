409711bc0de031906642d6cc91bb5c35905297fc
legregius <legregius@df8a077f-1820-0410-8776-e0b621f843e8>
2015-03-18 16:43:57 +0000

    reverted a change in SocketPair

--- src/gnelib-0_70/src/SocketPair.cpp
+++ src/gnelib-0_70/src/SocketPair.cpp
@@ -106,11 +106,12 @@ int SocketPair::rawRead(bool reliable, Buffer& buf) const {
 
   buf.clear();
   int read = nlRead( act, (NLvoid*)buf.getData(), (NLint)buf.getCapacity() );
-  
-  //this if checks prevents an exception being thrown under forceful disconnect
-  //which will leave the socket in a bad state
-  if(read > 0) buf.setLimit( read );
-
+  if (NL_INVALID == read) {
+     buf.clear();
+  }
+  else {
+     buf.setLimit( read );
+  }
   return read;
 }
 
