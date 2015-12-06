04d74526f7df0d4be4b343de5bf066dd77594142
legregius <legregius@df8a077f-1820-0410-8776-e0b621f843e8>
2015-12-06 18:15:05 +0000

    Fixed the build to work in linux and added a linux build script.

---
+++ buildLinux.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+echo boo
+. ../basebuild.sh
+echo who
+
+cmake .  ${DEFAULT_CMAKE_OPTIONS} -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" -DCMAKE_LD_FLAGS="${CMAKE_LD_FLAGS}" ${Boost_CMAKE_OPTIONS} -DCMAKE_INCLUDE_PATH:PATH=$EXT/include:$TBB_DIR/include -DCMAKE_LIBRARY_PATH:PATH=$TBB_DIR/lib -DDELTA3D_USE_EXTERNAL_OPENEXR=ON -DDELTA3D_BUILD_OPENVDB=OFF ${TBB_CMAKE_OPTIONS}  $@
+
+LD_LIBRARY_PATH="$PWD/dependencies/lib/dynamic" make -j$PROCESSORS || exit 1
+
+cd dependencies/include
+
+
+cp -av AL cppunit xercesc ode nl.h gnelib.h gnelib cal3d OpenEXR openvdb pcre* $EXT/include
+
+mv $EXT/include/AL $EXT/include/OpenAL
+
+cd ../lib/dynamic
+
+cp -av libopenvdb* libHalf* libIlm* libIex* libImath* libNL.* libalut* libcal3d.* libcppunit.* libgne.* libode.* libxerces-c* libzlib.dylib $EXT/lib
+
+cd ..
+
+cp -av static/libexpat.a static/libfreetype.a static/libglew.a static/libglfw.a static/libjpeg.a static/liblibpng.a static/liblua.a static/libpcre.a static/libSILLY.a  static/libtoluapp.a dynamic/libxerces-c* dynamic/libzlib.* $EXT/src/CEGUI-0.7.9/projects/Xcode/dependencies/lib
