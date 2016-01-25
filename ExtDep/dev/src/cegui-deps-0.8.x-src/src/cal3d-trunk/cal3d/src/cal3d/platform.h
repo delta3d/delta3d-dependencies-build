b5f98d742c78b70f8782dd921aec93c6edf39e50
legregius <legregius@df8a077f-1820-0410-8776-e0b621f843e8>
2016-01-25 14:59:46 +0000

    64 bit windows build fix.

--- ExtDep/dev/src/cegui-deps-0.8.x-src/src/cal3d-trunk/cal3d/src/cal3d/platform.h
+++ ExtDep/dev/src/cegui-deps-0.8.x-src/src/cal3d-trunk/cal3d/src/cal3d/platform.h
@@ -15,14 +15,17 @@
 // Compiler configuration                                                     //
 //****************************************************************************//
 
-#if defined(_WIN32) && defined(_WIN64) && !defined(__MINGW32__) && !defined(__CYGWIN__)
+#if defined(_WIN32) || defined(_WIN64) && !defined(__MINGW32__) && !defined(__CYGWIN__)
 #pragma warning(disable : 4251)
 #pragma warning(disable : 4786)
 #define snprintf _snprintf
 #endif
 
-#if !defined(_WIN32) && !defined(_WIN32) || defined(__MINGW32__) || defined(__CYGWIN__)
+#if (!defined(_WIN32) && !defined(_WIN64)) || defined(__MINGW32__) || defined(__CYGWIN__)
 #define stricmp strcasecmp
+#endif
+#if defined(_MSC_VER) && _MSC_VER >= 1600
+#define stricmp _stricmp
 #endif
 
 #if defined(_MSC_VER) && _MSC_VER <= 1200
